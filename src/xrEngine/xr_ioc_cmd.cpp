7d4293fb888952ae3e8bffe08fdc79b3822972ea
Searge <searge@ocean-labs.de>
2018-06-15 19:21:37 +0200

    Adjusted min & max values for snd_targets

--- src/xrEngine/xr_ioc_cmd.cpp
+++ src/xrEngine/xr_ioc_cmd.cpp
@@ -798,7 +798,7 @@ void CCC_Register()
     CMD1(CCC_SND_Restart, "snd_restart");
     CMD3(CCC_Mask, "snd_acceleration", &psSoundFlags, ss_Hardware);
     CMD3(CCC_Mask, "snd_efx", &psSoundFlags, ss_EAX);
-    CMD4(CCC_Integer, "snd_targets", &psSoundTargets, 50, 200);
+    CMD4(CCC_Integer, "snd_targets", &psSoundTargets, 16, 256);
     CMD4(CCC_Integer, "snd_cache_size", &psSoundCacheSizeMB, 8, 256);
 
 #ifdef DEBUG
